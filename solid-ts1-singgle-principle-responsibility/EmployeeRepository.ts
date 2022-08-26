2f685874e7eebbc44328e27c96882e18d55e9d05
zidniryi <zidnirydwan@gmail.com>
2022-08-26 15:17:52 +0700

    feat: add comment in Single-Responsibility Principle

--- solid-ts1-singgle-principle-responsibility/EmployeeRepository.ts
+++ solid-ts1-singgle-principle-responsibility/EmployeeRepository.ts
@@ -5,6 +5,7 @@ export class EmployeeRepository {
     // In real world you should separate in repository db
     // If in FE like for HTTP Request
     //  If in BE saving to DB SQL
+    // Log for run in index.ts
     console.log(employee, "Save TO DB");
   }
 }
